1c0f065c629e3c2704b1bdf578aeac7d8ca46069
Jasper Chang <jasperc8@gmail.com>
2017-04-01 00:44:45 +0800

    Add more templates

--- template/index.js
+++ template/index.js
@@ -1,7 +1,11 @@
 const TEXT = require('./text.json');
 const IMAGE = require('./image.json');
+const CAROUSEL = require('./carousel.json');
+const CAROUSEL_CONTENT = require('./carousel-content.json');
 
 module.exports = {
   TEXT,
-  IMAGE
+  IMAGE,
+  CAROUSEL,
+  CAROUSEL_CONTENT
 };
